34edf3b8ed3b6dad51f6c572d7abfc8f9b4335c3
WMostert1 <werner.mostert1@gmail.com>
2015-04-05 11:43:04 +0200

    Space schema

---
+++ models/space.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+var SpaceSchema = mongoose.Schema({
+    module_id           : String,           
+    registered_users    : [{ user_id: String }],           
+    academic_year       : String,
+    is_open           	: Boolean,
+    root_thread_id		: String
+});
+
+module.exports = mongoose.model("modules", ModuleSchema);
