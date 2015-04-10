ac9e106594a37ad14ecab7c7d57208c9b55615f1
Trevor Austin <taustin.swim@gmail.com>
2015-04-10 10:32:07 +0200

    Added a role weighting to allow for role  comparison.

--- models/role.js
+++ models/role.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 
 var RoleSchema = mongoose.Schema({
     role_id         : String,           /* The id of the role */
-    name            : String            /* The name of the role, as from LDAP */
+    name            : String,            /* The name of the role, as from LDAP */
+    role_weight     : Number            /* The weighting of the role, this is used for comparison of the roles*/
 });
 
 
