20fc71140dfedec968772f2853069d9dd0a5722c
Thinus Naude <thinusn@gmail.com>
2015-04-12 10:08:20 +0200

    updated

--- index.js
+++ index.js
@@ -2,23 +2,47 @@
  * Created by Thinus on 2015-04-11.
  */
 
+/**
+ * This is the object used to contain the loaded models
+ * @constructor
+ */
 function DS() {
-    //empty array to hold mongoose Schemas
+    /**
+     *  An empty array that will be used to hold the mongoose Schemas that are locatedin the ./models folder
+     */
     this.models = {};
 }
 
+/**
+ * Initiates all the mongoose Schemas that are located in the ./models folder.
+ * @param mongoose mongoose used in main app passed to this module.
+ */
 DS.prototype.init = function(mongoose) {
 
+    /**
+     * Prints out to the console once a connection has been established to the server.
+     */
     mongoose.connection.on('open', function (ref) {
         console.log('Connected to mongo server.');
     });
 
+    /**
+     * Prints out to the console if there has been a connection error.
+     */
     mongoose.connection.on('error', function (err) {
         console.log('Could not connect to mongo server!');
         console.log(err);
     });
 
+    /**
+     * Connects to the database
+     */
     mongoose.connect("mongodb://d3user:DdJXhhsd2@proximus.modulusmongo.net:27017/purYv9ib");
+
+
+    /*
+        Loads the required modules into the application.
+     */
     require('./models/appraisal');
     require('./models/module');
     require('./models/notification');
@@ -32,7 +56,11 @@ DS.prototype.init = function(mongoose) {
     require('./models/user');
     require('./models/user_subscription_settings_schema');
 
-    //This is just to make referencing the models easier within your apps, so you don't have to use strings. The model name you use here must match the name you used in the schema file
+    /*
+        This is just to make referencing the models easier within the buzz modules.
+        The model name you use here must match the name you used in the schema file.
+     */
+
     this.models = {
         appraisal: mongoose.model('appraisals'),
         module: mongoose.model('modules'),
@@ -46,8 +74,11 @@ DS.prototype.init = function(mongoose) {
         user: mongoose.model('users'),
         userSubscriptionSettings: mongoose.model('usersubscriptionsettings')
     }
+
 };
 
+//Creates an instance of the DS class
 var ds = new DS();
 
+//exports the class for use by buzz modules using this module
 module.exports = ds;
